b5f33a4f92e6a744837e8de3509ca7969bd67522
Mathias <mathias.lantean@gmail.com>
2019-09-14 15:37:59 -0300

    Creación del servidor (listener)

--- Obligatorio/ServerConsole/ServerConsole.cs
+++ Obligatorio/ServerConsole/ServerConsole.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServerLogic;
 
 namespace ServerConsole
 {
     class ServerConsole
     {
+        Server server = new Server();
         static void Main(string[] args)
         {
+            Server.Connect();
+
         }
     }
 }
