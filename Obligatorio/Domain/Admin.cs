f7070cb1dc223cf76c7c8259bfcd6a3a38c94b7b
Mathias <mathias.lantean@gmail.com>
2019-09-14 17:07:59 -0300

    Creación de las clases de dominio
    Se crean las clases referentes a nuestro domino del problema.

---
+++ Obligatorio/Domain/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Admin
+    {
+        public User User = new User() { Username = "admin", Password = "admin" };
+
+        public override bool Equals(object obj)
+        {
+            return this.User.Equals(((Admin)obj).User);
+        }
+    }
+}
